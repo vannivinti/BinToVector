55012261e5136e5be00543eb47864e13b9abfa13
vanni.vinti <vanni.vinti@gmail.com>
2015-06-15 16:18:28 +0200

    first commit

---
+++ BinToCVector/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace BinToCVector
+{
+    partial class BinToCVector
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Input Binary  file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "None";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(31, 93);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(100, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Number of equally sized C Vectors in output file";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 68);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "DO IT!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BinToCVector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 221);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "BinToCVector";
+            this.Text = "BinToCVector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BinToCVector/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+namespace BinToCVector
+{
+    public partial class BinToCVector : Form
+    {
+        public BinToCVector()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "bin files (*.bin)|*.bin|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = openFileDialog1.FileName;
+            }
+
+        }
+
+
+
+        // Wite to a file a byte array, using multiple lines if necessary. 
+        private void WriteMultiLineByteArray(byte[] bytes, string name, FileStream fsDestination)
+        {
+            const int rowSize = 20;
+            const string underLine = "/*--------------------------------*/";
+            int iter;
+            string stringLine;
+         
+
+            AddText(fsDestination, name + " = {\n");
+
+            for (iter = 0; iter < bytes.Length - rowSize; iter += rowSize)
+            {
+                AddText(fsDestination, "0x");
+                stringLine = BitConverter.ToString(bytes, iter, rowSize);
+                stringLine = stringLine.Replace("-", ", 0x");
+
+                AddText(fsDestination, stringLine);
+                AddText(fsDestination, ",\n");
+            }
+
+            AddText(fsDestination, "0x");
+            stringLine = BitConverter.ToString(bytes, iter);
+            stringLine = stringLine.Replace("-", ", 0x");
+
+            AddText(fsDestination, stringLine);     
+            AddText(fsDestination, "};\n\n\n" + underLine);
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int numBytesRead = 0;
+           
+
+            // open the binary source file
+            try
+            {
+
+                FileStream fsSource = new FileStream(label1.Text, FileMode.Open, FileAccess.Read);
+
+                // Read the source file into a byte array. 
+                byte[] bytes = new byte[fsSource.Length];
+                int numBytesToRead = (int)fsSource.Length;
+          
+
+                while (numBytesToRead > 0)
+                {
+                    // Read may return anything from 0 to numBytesToRead. 
+                    int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                    // Break when the end of the file is reached. 
+                    if (n == 0)
+                        break;
+
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+                }
+
+                fsSource.Close();
+
+                int i;
+                int byteOffset = 0;
+               
+                for (i = 0; i<numericUpDown1.Value; i++)
+                {
+                    byte[] data = new byte[numBytesRead / (int)numericUpDown1.Value];
+
+                    // open the text destination file
+                    FileStream fsDestination = new FileStream("BinaryVect" + i.ToString() + ".c", FileMode.Create, FileAccess.Write);
+                    for(int j=0; j<(numBytesRead/(int)numericUpDown1.Value); j++)
+                    {
+                        data[j] = bytes[byteOffset++];
+                    }
+
+                    WriteMultiLineByteArray(data, "#include <rtems.h>\n#include <bsp.h>\n\n\n\nuint8_t vector_" + i.ToString() + "[]", fsDestination);
+                    fsDestination.Close();
+                }
+
+                if( (numBytesRead % (int)numericUpDown1.Value) > 0)
+                {
+                    byte[] data = new byte[numBytesRead % (int)numericUpDown1.Value];
+
+                    FileStream fsDestination = new FileStream("BinaryVect" + i.ToString() + ".c", FileMode.Create, FileAccess.Write);
+                    for (int j = 0; j < (numBytesRead % (int)numericUpDown1.Value); j++)
+                    {
+                        data[j] = bytes[byteOffset++];
+                    }
+
+                    WriteMultiLineByteArray(data, "#include <rtems.h>\n#include <bsp.h>\n\n\n\nuint8_t vector_" + i.ToString() + "[]", fsDestination);
+                    fsDestination.Close();
+                }
+
+
+                MessageBox.Show("DONE!");
+
+            }
+            catch (FileNotFoundException ioEx)
+            {
+                MessageBox.Show(ioEx.Message);
+            }
+
+        }
+
+
+
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+
+    }
+}
